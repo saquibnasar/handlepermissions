7ecb50b84264b956e2e469295ca6b9c036741a63
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-11-20 15:29:02 +0530

    handle permissions

---
+++ src/app/hasPermision.ts
@@ -0,0 +1,21 @@
+export type User = { roles: Role[]; id: string };
+
+type Role = keyof typeof ROLES;
+type Permission = (typeof ROLES)[Role][number];
+
+const ROLES = {
+  admin: [
+    "view:comments",
+    "create:comments",
+    "update:comments",
+    "delete:comments",
+  ],
+  moderator: ["view:comments", "create:comments", "delete:comments"],
+  user: ["view:comments", "create:comments"],
+} as const;
+
+export function hasPermission(user: User, permission: Permission) {
+  return user.roles.some((role) =>
+    (ROLES[role] as readonly Permission[]).includes(permission)
+  );
+}
